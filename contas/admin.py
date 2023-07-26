31083cc96ba2382e811a132e21eb98500c8fbc68
Lussati Santos <lussatisantos10@gmail.com>
2023-07-26 13:26:27 +0100

    models e db.sqlite3

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria
+from .models import Transacao
 
+
+admin.site.register(Categoria)
+admin.site.register(Transacao)
 # Register your models here.
--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+
+class Transacao(models.Model):
+    data = models.DateTimeField(auto_now=True)
+    descricao = models.CharField(max_length=250)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Transacoes'
