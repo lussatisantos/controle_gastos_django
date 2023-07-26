b51ff46d116d6c8bdcd700f6c355f8fefd3297c5
Lussati Santos <lussatisantos10@gmail.com>
2023-07-26 13:46:32 +0100

    actualizacao do admin e o banco de dados

--- contas/models.py
+++ contas/models.py
@@ -4,12 +4,17 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
+
 class Transacao(models.Model):
-    data = models.DateTimeField(auto_now=True)
+    data = models.DateTimeField(auto_now_add=True)
     descricao = models.CharField(max_length=250)
-    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    #valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     observacoes = models.TextField(null=True, blank=True)
-
     class Meta:
         verbose_name_plural = 'Transacoes'
+
+    def __str__(self):
+        return self.descricao
