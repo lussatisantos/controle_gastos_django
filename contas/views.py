f9386286d22b23436f8c3ab4cbeef959e15f5419
Lussati Santos <lussatisantos10@gmail.com>
2023-07-27 13:57:25 +0100

    create

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Transacao
 from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacao'] = ['t1', 't2', 't3']
+    data ['now'] = datetime.datetime.now()
     #html = "<html><body> It is now %s.</body></html>" % now
     #return HttpResponse(html)
+    return render(request, 'contas/home.html', data)
 
-    return render(request, 'contas/home.html')
+def listagem(request):
+    data = {}
+    data['transacoes'] = 
+    return render(request, 'contas/listagem.html')
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home
+from contas.views import home, listagem
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('', listagem),
 ]
